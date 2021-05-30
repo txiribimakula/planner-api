17875290072b6fea8b7cfd21ebc5f6a68c5fd4c4
txiribimakula <pablotobalina@gmail.com>
2021-05-30 20:36:57 +0200

    Get plans from deserialized response.

---
+++ PlannerApi/Utils/Extensions.cs
@@ -0,0 +1,29 @@
+﻿using PlannerApi.Models;
+using System;
+using System.Collections.Generic;
+
+namespace PlannerApi.Utils
+{
+    public static class Extensions
+    {
+        public static IEnumerable<IEnumerable<Plan>> GetPlans(this WorkbookTableRowsResponse workbookTableRowsResponse) {
+            List<Plan> plans = new List<Plan>();
+            List<Plan> shortPlans = new List<Plan>();
+            List<Plan> midPlans = new List<Plan>();
+            List<Plan> longPlans = new List<Plan>();
+            foreach (var row in workbookTableRowsResponse.Rows) {
+                string planName = row.Values[0][0].ToString();
+                plans.Add(new Plan(planName));
+                shortPlans.Add(new Plan(planName, Convert.ToInt32(row.Values[0][1])));
+                midPlans.Add(new Plan(planName, Convert.ToInt32(row.Values[0][2])));
+                longPlans.Add(new Plan(planName, Convert.ToInt32(row.Values[0][3])));
+            }
+
+            IEnumerable<IEnumerable<Plan>> listOfPlans = new List<IEnumerable<Plan>>() {
+                plans, shortPlans, midPlans, longPlans
+            };
+
+            return listOfPlans;
+        }
+    }
+}
