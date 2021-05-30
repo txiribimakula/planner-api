8d8be9302511ac936fd943c9629d18b8d09d8002
txiribimakula <pablotobalina@gmail.com>
2021-05-30 20:36:33 +0200

    Model to deserialize response

---
+++ PlannerApi/Models/IWorkbookTableRowsResponse.cs
@@ -0,0 +1,20 @@
+﻿namespace PlannerApi.Models
+{
+    using Newtonsoft.Json;
+
+    public class WorkbookTableRowsResponse
+    {
+        [JsonProperty("value")]
+        public Value[] Rows { get; set; }
+    }
+
+    public class Value
+    {
+        [JsonProperty("index")]
+        public int Index { get; set; }
+
+        [JsonProperty("values")]
+        public object[][] Values { get; set; }
+    }
+
+}
