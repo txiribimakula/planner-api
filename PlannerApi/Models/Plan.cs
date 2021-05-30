b873a56a90e8d028a086e183bf14bbcbc7a82ed4
txiribimakula <pablotobalina@gmail.com>
2021-05-30 20:36:41 +0200

    Model to hold plans

--- PlannerApi/Models/Plan.cs
+++ PlannerApi/Models/Plan.cs
@@ -5,5 +5,10 @@
         public string Title { get; set; }
 
         public int Value { get; set; }
+
+        public Plan(string title, int value = 0) {
+            Title = title;
+            Value = value;
+        }
     }
 }
