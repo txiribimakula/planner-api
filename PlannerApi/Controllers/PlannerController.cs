cde49392ea2108f8376070b507b498cc1dd33d48
txiribimakula <pablotobalina@gmail.com>
2021-06-13 11:52:20 +0200

    Use GraphApi directly.

--- PlannerApi/Controllers/PlannerController.cs
+++ PlannerApi/Controllers/PlannerController.cs
@@ -3,6 +3,7 @@
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.Graph;
     using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
     using PlannerApi.Models;
     using PlannerApi.Utils;
     using System;
@@ -15,8 +16,6 @@
     [ApiController]
     public class PlannerController : ControllerBase
     {
-        private readonly string baseAddress = "https://graph.microsoft.com/v1.0/me/";
-
         [HttpGet("plans")]
         public async Task<Plan[][]> GetPlans() {
             GraphServiceClient graphClient = new GraphServiceClient(new AuthenticationProvider(Auth.GetAuthHeader(Request.Headers)));
@@ -32,9 +31,7 @@
         public async Task<Plan[][]> UpdatePlans(Plan[][] plans) {
             bool isAnythingPostedOrDeleted = false;
 
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri(baseAddress);
-            client.DefaultRequestHeaders.Authorization = Auth.GetAuthHeader(Request.Headers);
+            GraphServiceClient graphClient = new GraphServiceClient(new AuthenticationProvider(Auth.GetAuthHeader(Request.Headers)));
 
             var currentListsOfPlans = await GetPlans();
             var currentPlans = currentListsOfPlans.ElementAt(0);
@@ -42,7 +39,9 @@
             for (int i = currentPlans.Length - 1; i >= 0; i--) {
                 int index = Array.FindIndex(plans[0], plan => plan.Title == currentPlans[i].Title);
                 if (index == -1) {
-                    await client.DeleteAsync($"drive/items/EB4D21CF97FBA497!11746/workbook/tables/plans/rows/$/ItemAt(index={index})");
+                    await graphClient.Me.Drive.Items["EB4D21CF97FBA497!11746"].Workbook.Tables["plans"].Rows[$"ItemAt(index={i})"]
+                        .Request()
+                        .DeleteAsync();
                     isAnythingPostedOrDeleted = true;
                 }
             }
@@ -56,21 +55,17 @@
                 }
             }
             if (plansToPost.Count() > 0) {
-                var serialized = JsonConvert.SerializeObject(
-                    new Value() {
-                        Values = plansToPost.Select(plan => new object[] {
-                            plan.Title,
-                            Array.Find(plans[1], shortPlan => shortPlan.Title == plan.Title).Index,
-                            Array.Find(plans[2], midPlan => midPlan.Title == plan.Title).Index,
-                            Array.Find(plans[3], longPlan => longPlan.Title == plan.Title).Index
-                        }).ToArray()
-                    }
-                );
-                var postRequestContent = new StringContent(serialized);
-                var postResponse = await client.PostAsync($"drive/items/EB4D21CF97FBA497!11746/workbook/tables/plans/rows/add", postRequestContent);
-                var postResponseContent = await postResponse.Content.ReadAsStringAsync();
+                var values = JToken.FromObject(plansToPost.Select(plan => new object[] {
+                    plan.Title,
+                    Array.Find(plans[1], shortPlan => shortPlan.Title == plan.Title).Index,
+                    Array.Find(plans[2], midPlan => midPlan.Title == plan.Title).Index,
+                    Array.Find(plans[3], longPlan => longPlan.Title == plan.Title).Index
+                }));
 
-                WorkbookTableRowsResponse rowsResponse = JsonConvert.DeserializeObject<WorkbookTableRowsResponse>(postResponseContent);
+                var postResponse = await graphClient.Me.Drive.Items["EB4D21CF97FBA497!11746"].Workbook.Tables["plans"].Rows
+                    .Add(null, values)
+                    .Request()
+                    .PostAsync();
             }
 
             if (!isAnythingPostedOrDeleted) {
@@ -80,18 +75,18 @@
                         || !Array.Find(plans[2], plan => plan.Title == plans[0][i].Title).Equals(currentListsOfPlans[2][i])
                         || !Array.Find(plans[3], plan => plan.Title == plans[0][i].Title).Equals(currentListsOfPlans[3][i])) {
                         var serialized = JsonConvert.SerializeObject(
-                        new Value() {
-                            Values = new object[][] {
+                        new object[][] {
                             new object [] {
                                 plans[0][i].Title,
                                 Array.Find(plans[1], shortPlan => shortPlan.Title == plans[0][i].Title).Index,
                                 Array.Find(plans[2], midPlan => midPlan.Title == plans[0][i].Title).Index,
                                 Array.Find(plans[3], longPlan => longPlan.Title == plans[0][i].Title).Index
-                            }
-                            }
-                        });
+                            } });
                         var patchRequestContent = new StringContent(serialized);
-                        await client.PatchAsync($"drive/items/EB4D21CF97FBA497!11746/workbook/tables/plans/rows/$/ItemAt(index={i})", patchRequestContent);
+
+                        await graphClient.Me.Drive.Items["EB4D21CF97FBA497!11746"].Workbook.Tables["plans"].Rows[$"ItemAt(index={i})"]
+                            .Request()
+                            .UpdateAsync(new WorkbookTableRow() { Values = JToken.Parse(serialized) });
                     }
                 }
             }
